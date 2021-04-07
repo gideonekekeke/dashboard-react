b6c25cf66a67e3122b7ee692f9408fec57ce0a12
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-04-08 00:31:50 +0100

    dashboard

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import MaterialDash from "../src/Component/MaterialDash";
+
+function App() {
+  return (
+    <div>
+      <MaterialDash />
+    </div>
+  );
+}
+
+export default App;
